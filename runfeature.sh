8e7743ee8dd54cb25cffb92ce7a44f967ec89e98
Ioannis G. Tsoulos <sheridan@core>
2023-09-05 19:29:00 +0300

    add doc

--- runfeature.sh
+++ runfeature.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DATAPATH=~//Desktop/ERGASIES/FeatureConstruction2/datasets/tenfolding/
+DATAPATH=~/Desktop/ERGASIES/FeatureConstruction2/datasets/tenfolding/
 NODES=10
 GENS=200
 MARGIN=1024
-./IntervalGenetic  --localmethod=genetic --intervalmethod=integer --filename=PROBLEMS/Feature/libFeature.so --param=features --value=2 --param=rightMargin --value=$MARGIN --param=trainName --value=$DATAPATH/$1.train --param=testName --value=$DATAPATH/$1.test --param=nodes --value=$NODES --generations=$GENS --seed=2 --threads=8 2>xx.out
+./IntervalGenetic  --localmethod=genetic --intervalmethod=grammar --filename=PROBLEMS/Feature/libFeature.so --param=features --value=2 --param=rightMargin --value=$MARGIN --param=trainName --value=$DATAPATH/$1.train --param=testName --value=$DATAPATH/$1.test --param=nodes --value=$NODES --generations=$GENS --seed=2 --threads=8 
