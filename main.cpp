88c9cd62eca6859f759482eaab88bacf3fa7e736
Ioannis G. Tsoulos <itsoulos@gmail.com>
2025-02-24 07:20:34 +0200

    neural fixes

--- main.cpp
+++ main.cpp
@@ -341,8 +341,8 @@ int main(int argc,char **argv)
         bestMargin = p.getMargins();
         for(int i=0;i<bestMargin.size();i++)
         {
-            bestMargin[i]=Interval(-2.0*fabs(bestx[i]),
-                                   2.0*fabs(bestx[i]));
+            bestMargin[i]=Interval(-1.0*fabs(bestx[i]),
+                                   1.0*fabs(bestx[i]));
 
             printf("Margin[%d]=%lf,%lf\n",i,bestMargin[i].leftValue(),
                    bestMargin[i].rightValue());
--- runmlp.sh
+++ runmlp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DATAPATH=~/Desktop/ERGASIES/FeatureConstruction2/datasets/tenfolding/
 NODES=10
-GENS=200
+GENS=500
 
 ./IntervalGenetic  --localmethod=genetic --intervalmethod=genetic --filename=PROBLEMS/libnntest.so --param=trainName --value=$DATAPATH/$1.train --param=testName --value=$DATAPATH/$1.test --param=nodes --value=$NODES --generations=$GENS  --threads=8
 
